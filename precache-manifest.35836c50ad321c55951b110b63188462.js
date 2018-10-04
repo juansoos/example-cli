1b549556c17faa67d8c8e12e1e5d3b93884d76a2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-04 13:31:28 +0000

    Deploy juansoos/example-cli to github.com/juansoos/example-cli.git:gh-pages

---
+++ precache-manifest.35836c50ad321c55951b110b63188462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "656523aa851306713698",
+    "url": "/static/js/main.656523aa.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "656523aa851306713698",
+    "url": "/static/css/main.c67e003f.chunk.css"
+  },
+  {
+    "revision": "bd33f93217f9e879e609694e295f87fa",
+    "url": "/index.html"
+  }
+];
